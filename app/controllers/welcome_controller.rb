7dd54a10d14026a95f301207144398488fe52314
CaseyKelly <casey.kelly@colorado.edu>
2015-03-11 13:25:52 -0600

    User show page lists user details

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
 
   def index
+    @new = GithubFetcher.new
+    @user = @new.client.user
+    @respositories = @new.client.repositories
   end
-  
+
+  def userinfo
+    @new = GithubFetcher.new
+    @user = @new.client.user(params[:username])
+    @userinfo = @user.user_info(params[:username])
+  end
+
 end
--- app/models/github_fetcher.rb
+++ app/models/github_fetcher.rb
@@ -1,8 +1,13 @@
 class GithubFetcher
 
-  attr_reader: :client
+  attr_reader :client
 
-  def intialize
+  def initialize
     @client = Octokit::Client.new(:access_token => ENV['ACCESS_TOKEN'])
   end
+
+  def user_info(username)
+    @client.user(username)
+  end
+
 end
