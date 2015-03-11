be4febb76329227985ae593e2762ca607932eedf
CaseyKelly <casey.kelly@colorado.edu>
2015-03-11 16:40:58 -0600

    repos link back to github

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,14 @@ class WelcomeController < ApplicationController
   def index
     @new = GithubFetcher.new
     @user = @new.client.user
-    @respositories = @new.client.repositories
+    @repositories = @new.client.repositories
   end
 
   def userinfo
     @new = GithubFetcher.new
     @user = @new.client.user(params[:username])
     @userinfo = @user.user_info(params[:username])
-    @respositories = @new.client.repositories(params[:username])
+    @repositories = @new.client.repositories(params[:username])
   end
 
 end
